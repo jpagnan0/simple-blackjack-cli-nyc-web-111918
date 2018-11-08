f9a5c3ebe1030d2ecdc8a116d4de878b8a6dee4e
Adam Saland <a.jpagnano@gmail.com>
2018-11-08 20:56:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,9 +55,10 @@ end
 
 def runner
   # code runner here
-  welcome  
+  
   card_total = initial_round
   while card_total < 21
+    welcome  
     card_total = hit?(card_total)
     card_total = display_card_total(card_total)
   end
